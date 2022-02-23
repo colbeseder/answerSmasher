46663139d6153bbc4a26897320d06551bbf5bae8
Bryan Appleby <bryanjappleby@gmail.com>
2022-02-23 22:24:14 +0200

    Kubernetesize the frontent

--- startLocal.sh
+++ startLocal.sh
@@ -1,4 +1,5 @@
 minikube start
 kubectl apply -f k8s-files
-sleep 120
-kubectl port-forward service/apigate 3000:3000 --namespace backend-namespace
+sleep 200
+kubectl port-forward service/apigate 3000:3000 --namespace backend-namespace &
+kubectl port-forward service/ui-service 3001:3001 &
