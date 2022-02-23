5682420ebf733fa8b9d3956aec1a6e80040e8263
Bryan Appleby <bryanjappleby@gmail.com>
2022-02-23 23:31:09 +0200

    Clean up build scripts

--- build.sh
+++
@@ -1,8 +0,0 @@
-docker build --tag colbeseder/smasherbackend:latest -f backend/Dockerfile .
-docker push colbeseder/smasherbackend:latest
-
-docker build --tag colbeseder/smasherfrontend:latest -f frontend/Dockerfile .
-docker push colbeseder/smasherfrontend:latest
-
-docker build --tag colbeseder/smasher-entry-prep:latest -f prepareEntries .
-docker push colbeseder/smasher-entry-prep:latest
