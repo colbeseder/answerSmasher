9fa3b6dab4794daaa5ec5a4241254b250cde4d99
Bryan Appleby <bryanjappleby@gmail.com>
2022-02-28 12:37:10 +0200

    Reorganize static file structure

--- frontend/app.js
+++ frontend/app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.use("/static", express.static('static'))
+app.use("/scripts", express.static('static/scripts'))
+app.use("/styles", express.static('static/styles'))
+app.use("/JSX", express.static('static/JSX'))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/static/index.html');
