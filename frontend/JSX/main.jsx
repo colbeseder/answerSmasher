a6d28de9fecad115a4e4e26f9a4c8a6b92d63eb9
Bryan Appleby <bryanjappleby@gmail.com>
2022-03-01 12:26:02 +0200

    Fix quiz button

--- frontend/JSX/main.jsx
+++ frontend/JSX/main.jsx
@@ -1,77 +1,4 @@
 const e = React.createElement;
-const apiUrl = "http://api." + location.host
-
-
-function leadCapital(s){
-    return s.replace(/^./, x => x.toUpperCase())
-}
-
-function combineSpelling(a, b){
-    if (!a) {
-        return b;
-    }
-    if (!b){
-        return leadCapital(a);
-    }
-    var joint = b.charAt(0);
-    if (a.indexOf(joint) === -1){
-        return `${leadCapital(a)}-${leadCapital(b)}`;
-    }
-    else {
-        var parts = a.split(joint);
-        parts.pop();
-        return leadCapital(parts.join(joint) + b)
-    }
-}
-
-function createDigest(smash){
-    return btoa(smash.firstAnswer + "," + smash.secondAnswer);
-}
-
-function removeBrackets(s){
-    return s.replace(/\([^)]*\)\s*/g, '');
-}
-
-function leadingLower(s){
-    return s.replace(/^\s*[A-Z]/, x => x.toLowerCase());
-}
-
-var breakers = "(,|\\band\\b|\\bor\\b|\\bwith\\b|\\bfor\\b|\\bof\\b|\\bin\\b|\\bis\\b)";
-
-function combineDef(a, b){
-    var joiner = ' ';
-    var re = new RegExp(breakers, 'i');
-    if (!re.test(a) && !re.test(b)){
-        joiner = ' with ';
-    }
-
-    return removeBrackets(a).replace(new RegExp("^(.*?)" + breakers + ".*$", 'i'), '$1$2') +
-            joiner +
-            leadingLower(removeBrackets(b)).replace(new RegExp(".*" + breakers, 'i'), '');
-}
-
-function getSmashfromDigest(digest){
-        fetch(apiUrl + "/api/combine/" + digest)
-            .then(r => r.json())
-            .then(smash => {
-                if (smash.firstAnswer) {
-                    elem.setState(smash)
-                }
-                else {
-                    next();
-                }
-            })
-            .catch(er => next());
-}
-
-function next(){
-    fetch(apiUrl + "/api/smash").then(r => r.json())
-        .then(smash => { 
-            elem.setState(smash);
-            window.history.pushState('', '', '?d=' + createDigest(smash));
-         })
-        .catch();
-}
 
 class QuoteZone extends React.Component {
   constructor(props) {
@@ -91,8 +18,8 @@ class QuoteZone extends React.Component {
             <div id="parts"><span id="part1">{this.state.firstAnswer}</span> <span id="part2">{this.state.secondAnswer}</span></div>
             <div id="reactContainer">
                 <span id="answer">{combineSpelling(this.state.firstAnswer, this.state.secondAnswer)}</span><br></br>
-                <span id="IPA">/{this.state.pronounciation}/</span><br />
-                <div id="meaning">1. {combineDef(this.state.firstClue, this.state.secondClue)}</div><br></br>
+                <span id="IPA">{this.state.pronounciation ? '/' + this.state.pronounciation '/' : ''}</span><br />
+                <div id="meaning">{combineDef(this.state.firstClue, this.state.secondClue)}</div><br></br>
                 <div id="buttonContainer"><button onClick={x=>{next(this)}}>Next Smash</button></div>
             </div>
         </div>
