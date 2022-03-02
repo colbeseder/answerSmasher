db6001fd3b7b3589aa907d6bf9e14f9ba523ab0e
Bryan Appleby <bryanjappleby@gmail.com>
2022-03-02 19:01:39 +0200

    Comment Clean up

--- backend/start.sh
+++ backend/start.sh
@@ -1,8 +1,3 @@
 source constants.sh
 
-# Load the raw words into the database for prepare the entries
-#grep "^[a-z]\{3,\}" "resources/common-words.txt" > resources/ordered_titles
-#cat resources/ordered_titles | shuf > resources/titles
-#sleep 3
-#node queueTitles.js &
 node app.js
