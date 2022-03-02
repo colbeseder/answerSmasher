e7209a35def94d354ab3c67b055918fc7166c34a
Bryan <bryanjappleby@gmail.com>
2022-03-02 17:20:35 +0200

    Added header (#23)

---
+++ frontend/JSX/footer.jsx
@@ -0,0 +1,31 @@
+
+class FooterZone extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+        <div id="foot">
+            <a href="/">
+                <img className="navButton" src="/static/icons/tennis-blue.svg" />
+                Smashes
+            </a>
+            <a href="/quiz">
+                <img className="navButton" src="/static/icons/brain-user-blue.svg" />
+                quiz
+            </a>
+            
+            <span id="credit">
+                Created by 
+                <a href="https://twitter.com/bryapp">
+                    @bryapp
+                </a>
+            </span>
+        </div>
+    )
+  }
+}
+
+var footerElem = ReactDOM.render(React.createElement(FooterZone), document.querySelector('#footerContainer'));
