02b45a26d0f4c26d5e02b82fb75cee408c700004
Bryan Appleby <bryanjappleby@gmail.com>
2022-03-03 19:38:38 +0200

    Add congratulations image

--- frontend/JSX/quiz.jsx
+++ frontend/JSX/quiz.jsx
@@ -24,7 +24,8 @@ class QuoteZone extends React.Component {
             <div id="reactContainer" className={`${this.state.isCorrect ? "correct" : ""} ${this.state.isRevealed ? "revealed": ""}`}>
                 <div id="answerBlock">
                     <span id="answer">{this.state.isCorrect ? combineSpelling(this.state.firstAnswer, this.state.secondAnswer) 
-                        : combineSpelling(this.state.guess1, this.state.guess2) || ' '}</span><br></br>
+                        : combineSpelling(this.state.guess1, this.state.guess2) || ' '}</span>
+                        <img className={`${this.state.isCorrect && !this.state.isRevealed ? '' : 'hidden'}`} src="/static/icons/smiley-wink-blue.svg" id="correctImage" /><br />
                     <span id="IPA">{this.state.isCorrect ? '/' + this.state.pronounciation + '/' : ''}</span><br />
                     <div id="meaning">{this.state.isCorrect ? combineDef(this.state.firstClue, this.state.secondClue) : ''}</div><br></br>
                 </div>
