d61a67880a228b36a05f313f29935473d66d0079
Bryan Appleby <bryanjappleby@gmail.com>
2022-03-03 14:02:50 +0200

    Avoid slurs

--- frontend/JSX/main.jsx
+++ frontend/JSX/main.jsx
@@ -15,7 +15,7 @@ class QuoteZone extends React.Component {
   render() {
     return (
         <div>
-            <div id="parts"><span id="part1">{this.state.firstAnswer}</span> <span id="part2">{this.state.secondAnswer}</span></div>
+            <div id="parts"><span id="part1">{this.state.firstAnswer}</span> <span id="part2">{this.state.secondAnswer}&nbsp;</span></div>
             <div id="reactContainer" onClick={next} onKeyPress={nextOnEnter}>
                 <span id="answer">{combineSpelling(this.state.firstAnswer, this.state.secondAnswer)}</span><br></br>
                 <span id="IPA">{this.state.pronounciation ? '/' + this.state.pronounciation + '/' : ''}</span><br />
