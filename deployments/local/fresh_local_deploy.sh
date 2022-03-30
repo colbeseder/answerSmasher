9dbd7fc233666c7bbadfc37a1e87ee8cfc3a29b8
Bryan Appleby <bryanjappleby@gmail.com>
2022-03-30 18:30:05 +0300

    deploy mongo first

--- deployments/local/fresh_local_deploy.sh
+++ deployments/local/fresh_local_deploy.sh
@@ -1,4 +1,8 @@
+minikube delete
 minikube start
+
+kubectl apply -f ./deployments/local/mongo.yml
+
 eval $(minikube docker-env)
 minikube addons enable ingress
 minikube addons enable ingress-dns
@@ -12,5 +16,7 @@ docker build --tag colbeseder/smasher-entry-prep:local -f prepareEntries/Dockerf
 kubectl apply -f ./deployments/local/smasher-namespace.yml
 kubectl apply -f ../secrets/local/secrets.yml
 kubectl apply -f ./deployments/local
+
+sleep 30
 kubectl port-forward svc/apigate 30182:80 --namespace smasher-ns &
 minikube service ui-service --namespace smasher-ns
