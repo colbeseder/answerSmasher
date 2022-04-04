51bad2f50bfbae7d7b0c8659199be21199c77415
Bryan Appleby <bryanjappleby@gmail.com>
2022-04-04 18:15:02 +0300

    Fix: IS_PROD flag

--- frontend/start.sh
+++ frontend/start.sh
@@ -2,7 +2,8 @@ source constants.sh
 
 
 # Set api URL to the local version
-if [ $IS_LOCAL ] ; then echo "window.apiUrl = 'http://127.0.0.1:30182'; " > del0 ;
+if [ "$IS_PROD" = "no" ] ; then echo "window.apiUrl = 'http://127.0.0.1:30182'; " > del0 ;
+echo "Local deployment"
 cat del0 ./static/scripts/common.js > del1 ;
 mv del1 ./static/scripts/common.js
 fi
