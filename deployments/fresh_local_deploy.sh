d0e9e62129266670ddffc271a8225de9d5c34f41
Bryan <bryanjappleby@gmail.com>
2022-04-04 19:36:26 +0300

    Helm-ize production deployment (#34)
    * Deploy local and dev with same chart
    
    * fix Fresh local Deploy script
    
    * Clean up helm chart directory structure
    
    * mongo.yml from new location

--- deployments/fresh_local_deploy.sh
+++ deployments/fresh_local_deploy.sh
@@ -6,7 +6,7 @@ minikube addons enable ingress
 minikube addons enable ingress-dns
 
 minikube image load mongo
-kubectl apply -f ./deployments/local/mongo.yml
+kubectl apply -f ./deployments/mongo.yml
 
 npm install package.json
 npm run build --prefix ./frontend
@@ -15,8 +15,7 @@ docker build --tag colbeseder/smasherfrontend:local -f frontend/Dockerfile .
 docker build --tag colbeseder/smasherbackend:local -f backend/Dockerfile .
 docker build --tag colbeseder/smasher-entry-prep:local -f prepareEntries/Dockerfile .
 
-cd ./deployments/local/localchart
-helm secrets install yo-yo . -n smasher-ns-local --create-namespace --values ../secrets.yml
+helm secrets install dev ./deployments/chart -n smasher-ns-local --create-namespace --values ./deployments/local_secrets.yml --values ./deployments/local_values.yaml
 
 sleep 30
 kubectl port-forward svc/apigate 30182:80 --namespace smasher-ns-local &
