72c874aee0adf7321fc2ab4f7c7718178a4b0f24
Bryan Appleby <bryanjappleby@gmail.com>
2022-04-06 00:26:34 +0300

    word picker API

--- combiner/src/app.py
+++ combiner/src/app.py
@@ -8,6 +8,13 @@ from flask_restful import Resource, Api, reqparse
 # Load English tokenizer, tagger, parser and NER
 nlp = spacy.load("en_core_web_sm")
 
+def pickWord(text):
+    doc = nlp(text)
+    nouns = [chunk.text for chunk in doc.noun_chunks]
+    if len(nouns) == 0:
+        return ''
+    return nouns[-1]
+
 def combineDefs(text1, text2):
     doc1 = nlp(text1)
     doc2 = nlp(text2)
@@ -38,11 +45,21 @@ class Combiner(Resource):
         text2 = json_data['text2']
         return jsonify(result=combineDefs(text1, text2))
 
+class WordPicker(Resource):
+    def __init__(self):
+        pass
+    def post(self):
+        json_data = request.get_json(force=True)
+        text = json_data['text']
+        
+        return jsonify(result=pickWord(text))
+
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Combiner, '/combine')
 api.add_resource(Status, '/status')
+api.add_resource(WordPicker, '/pick')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)  # run our Flask app
--- combiner/src/start.sh
+++ combiner/src/start.sh
@@ -1,2 +1,1 @@
-python3 -m spacy download en_core_web_sm
 python3 app.py
