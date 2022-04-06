f1053138c0133921838582e7f1c5fad89d7e7fa9
Bryan Appleby <bryanjappleby@gmail.com>
2022-04-06 22:13:57 +0300

    Answerblock font should be Actor

--- frontend/JSX/main.jsx
+++ frontend/JSX/main.jsx
@@ -19,10 +19,12 @@ class QuoteZone extends React.Component {
         <div className={`${this.state.firstAnswer ? "" : "hidden"} `}>
             <div id="parts"><span id="part1">{this.state.firstAnswer}</span> <span id="part2">{this.state.secondAnswer}&nbsp;</span></div>
             <div id="reactContainer" onClick={next} onKeyPress={nextOnEnter}>
+            <div id="answerBlock">
                 <span id="answer">{combineSpelling(this.state.firstAnswer, this.state.secondAnswer)}</span><br></br>
                 <span id="IPA">{this.state.pronounciation ? '/' + this.state.pronounciation + '/' : ''}</span><br />
                 <div id="meaning">{combineDef(this.state.firstClue, this.state.secondClue, this.state.firstTarget, this.state.secondTarget)}</div>
-                <div id="buttonContainer"><button>Generate</button></div>
+            </div>
+            <div id="buttonContainer"><button>Generate</button></div>
             </div>
         </div>
     )
