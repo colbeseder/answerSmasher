5935fc2935ae3dff5c77edbe6cdeccbea2b50881
Bryan Appleby <bryanjappleby@gmail.com>
2022-04-10 12:06:16 +0300

    Add challenges

--- backend/getChallenge.js
+++ backend/getChallenge.js
@@ -9,6 +9,24 @@ function getChallenge() {
     return challenges[getChallengeNumber() % challenges.length];
 }
 
+function padRight(str, p){
+    if (str.length >= p){
+        return str;
+    }
+    var x = p - str.length;
+    var pad = new Array(x +1).join(' ');
+    return str + pad;
+}
+
+function showAllChallenges(){
+    var days = 0;
+    for (var days = 0; days < challenges.length; days++){
+        var c = challenges[days];
+        var pair = atob(c);
+        console.log(`${new Date(EPOCH + 8.64e7 ).toDateString()}: ${padRight('"' + pair + '"', 25)}\t http://answersmasher.com/quiz?d=${c}`);
+    }
+}
+
 var challenges = [
     "Z2xvc3NhcnksYXJlbmE=",
     "Y2hvY29sYXRlLGxpdGVyYWN5",
@@ -17,6 +35,7 @@ var challenges = [
     "bmV2ZXJ0aGVsZXNzLGxlc3Nlcg==",
     "c3ByaW50LGludGVyZmFjZQ==",
     "Y2F0aG9saWMsbGlja2luZw==",
+    "UmljaGFyZCBPc21hbixtYW5vcg==",
     "dGFyZ2V0LGd1aXRhcg==",
     "ZHJpbmssaW5jcmVkaWJsZQ==",
     "c3F1ZWFtaXNoLG1pc3Npb24=",
@@ -28,7 +47,16 @@ var challenges = [
     "Z3JpbixyaW5zZQ==",
     "cmVtYXJrLGFyY2FkZQ==",
     "d2hhbGUsYWxpZW4=",
-    "bWFybW90LG1hdHVyZQ=="
+    "bWFybW90LG1hdHVyZQ==",
+    "d2hpbXNpY2FsLGNvbGxlY3RpYmxl",
+    "cHJvdGVzdCxlc3R1YXJ5",
+    "Y2FsbCxhbHJlYWR5",
+    "c2ltdWx0YW5lb3VzbHksc2xlZXA",
+    "dHJhZmZpYyxmaWN0aW9u"
 ];
 
 module.exports = getChallenge;
+
+if (require.main === module) {
+    showAllChallenges();
+}
