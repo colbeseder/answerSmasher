aedf3e71a2b832f73a1b1d8a92d5b0779231a5d8
bryanappleby <bryan.appleby@kaymera.com>
2023-03-20 14:29:18 +0200

    Re-theme modals

--- frontend/JSX/help.jsx
+++ frontend/JSX/help.jsx
@@ -13,7 +13,7 @@ class HelpZone extends React.Component {
             <img className="navButton closeMenu" src="/static/icons/close.svg" onClick={_=>toggleHelp(false)} />
             <h1>What's an AnswerSmash?</h1>
             <p>You can make an AnswerSmash by combining two words with overlapping sounds. 
-              For example <i>Bowtie</i> and <i>Tiger</i> become <i><a href="/?d=Ym93dGllLHRpZ2Vy">Bowtiger</a></i></p>
+              For example <i>Bowtie</i> and <i>Tiger</i> become <i><a href="/quiz?d=Ym93dGllLHRpZ2Vy">Bowtiger</a></i></p>
             <h1>How to Play</h1>
             <p>The <a href="/quiz">Quiz page</a> will give you two definitions, whose answers combine to form an AnswerSmash</p>
             <p>The <a href="/daily">Smash of the Day</a> gives you the day's AnswerSmasher challenge. Can you get it before your friends? </p>
