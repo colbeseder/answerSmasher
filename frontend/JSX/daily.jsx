ebdf2f1b76b1e77f1ed593a3e965a3472a427f6c
bryanappleby <bryan.appleby@kaymera.com>
2023-03-26 23:12:45 +0300

    Daily page to match quiz

--- frontend/JSX/daily.jsx
+++ frontend/JSX/daily.jsx
@@ -24,28 +24,30 @@ class QuoteZone extends React.Component {
     return (
         <div>
             <div id="reactContainer" className={`${this.state.isCorrect ? "correct" : ""} ${this.state.isRevealed ? "revealed": ""}`}>
+                <div id="guesses">
+                    <div className="guessBox" id="guessBox1">
+                        <div>{cleanClue(this.state.firstClue, 3)}</div><br />
+                        <div>{cleanClue(this.state.secondClue, 3)}</div><br />
+                    </div>
+                </div>
+
                 <div id="answerBlock">
                     <span id="answer">{this.state.isCorrect ? combineSpelling(this.state.firstAnswer, this.state.secondAnswer) 
                         : combineSpelling(this.state.guess1, this.state.guess2) || ' '}</span>
                         <img className={`${this.state.isCorrect && !this.state.isRevealed ? '' : 'shrunk'}`} src="/static/icons/check-mark.svg" id="correctImage" /><br />
                     <span id="IPA">{this.state.isCorrect ? '/' + this.state.pronounciation + '/' : ''}</span><br />
-                    <div id="meaning">{this.state.isCorrect ? combineDef(this.state.firstClue, this.state.secondClue, this.state.firstTarget, this.state.secondTarget) : ''}</div><br></br>
                 </div>
-                <div id="guesses">
-                    <div className="guessBox" id="guessBox1">
-                        <div>{cleanClue(this.state.firstClue, 3)}</div><br />
+
+                <div className="guessBox" id="answerBox">
                         <input id="guess1" onKeyPress={nextOnEnter} onInput={ev => {this.setState(getGuesses(), checkSmash)}} />
-                    </div>
-                    <div className="guessBox" id="guessBox2">
-                        <div>{cleanClue(this.state.secondClue, 3)}</div><br />
                         <input id="guess2" onKeyPress={nextOnEnter} onInput={ev => {this.setState(getGuesses(), checkSmash)}} />
                     </div>
-                </div>
-                <br /><br />
+                    <br /><br />
             </div>
         </div>
     )
   }
+
 }
 
 const domContainer = document.querySelector('#root');
