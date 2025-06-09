d87fe9b651e58d55c07260a60f307e238f12f1b2
bryanappleby <bryan.appleby@kaymera.com>
2025-06-09 19:03:20 +0300

    Homepage redirects to /quiz

--- frontend/app.js
+++ frontend/app.js
@@ -1,3 +1,5 @@
+
+
 const path = require('path');
 const express = require('express');
 const app = express();
@@ -6,7 +8,8 @@ const port = 3001;
 app.use("/static", express.static(path.join(__dirname, './static')))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/static/quiz.html');
+  res.redirect('/quiz');
+  //res.sendFile(__dirname + '/static/quiz.html');
 })
 
 app.get('/quiz', (req, res) => {
